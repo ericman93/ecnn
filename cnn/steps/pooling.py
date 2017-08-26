ec8d4be0d6ce81177a988d2321938598ef217af2
Eric <ericfeldman93@gmail.com>
2017-08-26 18:09:20 +0300

    fix pooling

--- cnn/steps/pooling.py
+++ cnn/steps/pooling.py
@@ -11,41 +11,55 @@ class PoolingStep(BasicStep):
     def forward_propagation(self, input):
         self.inputs = input
         self.z = []
-        return self.__get_pooled(input)
+        self.a = self.__get_pooled(input)
+        return self.a
 
     def back_prop(self, delta, learnin_rate):
+        flatten_delta = delta.reshape(delta.size)
         delta_i = 0
         final_delta = []
 
-        for p in self.z:
+        for p in self.z.reshape(self.z.size):
             if p == 1:
-                final_delta.append(delta[delta_i])
+                final_delta.append(flatten_delta[delta_i])
                 delta_i += 1
             else:
                 final_delta.append(0)
 
-        return np.array(final_delta)
+        return np.array(final_delta).reshape(self.z.shape)
 
     def __get_pooled(self, input):
         input_hight = input.shape[1]
         input_widht = input.shape[2]
 
         layers = []
+        z = []
+
         for layer_index in range(0, input.shape[0]):
             layer = []
+
             for i in range(0, input_hight, self.size):
                 row = []
+
                 for j in range(0, input_widht, self.size):
                     bulk = input[layer_index, i: i + self.size, j: j + self.size]
+                    # z = np.copy(bulk)
+
                     pooled = self.pool(bulk)
                     row.append(pooled)
 
-                    self.z += list(self.__get_z_bulk(bulk, pooled))
+                    # If two values have the pooled value, two of them will mark as 1
+                    # pooled_bulk = [1 if v == pooled else 0 for v in set(bulk.reshape(bulk.size))]
+                    # pooled_bulk += [0] * (self.size * self.size - len(pooled_bulk)) # if I have multiple items with the same value
+
+                    z += self.__get_z_bulk(bulk,pooled)
+
 
                 layer.append(row)
 
             layers.append(layer)
 
+        self.z = np.array(z).reshape(self.inputs.shape)
         return np.array(layers)
 
     def __get_z_bulk(self, bulk, pooled):
